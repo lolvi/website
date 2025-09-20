96306112e20f003883dbecc7fc254adbe9756eb3
Javier Jaimes <javier@loolvidepagar.co>
2025-09-20 12:42:16 +0200

    Add privacy policy page component

---
+++ src/app/privacidad/page.tsx
@@ -0,0 +1,281 @@
+import React from "react";
+
+export default function Privacidad() {
+  return (
+    <>
+      <section className="relative z-10 bg-transparent text-center">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          <div className="lg:text-center">
+            <h2 className="text-base inline-block p-2 text-white bg-[var(--color-primary)] font-semibold font-droid tracking-wide uppercase">
+              Política de Privacidad
+            </h2>
+            <div>
+              <p className="mt-2 inline-block font-droid font-bold text-3xl leading-8 tracking-tight text-gray-900 sm:text-4xl">
+                Tratamiento de datos personales
+              </p>
+              <p className="mt-4 inline-block font-droid max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Nuestra intención es ser 100% transparentes en el manejo de tus personales, escríbenos si ves alguna inconsistencia, duda o comentario.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative z-10 py-12 px-6 sm:px-2 bg-transparent">
+        <div className="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-8 md:gap-y-10">
+          <div className="relative text-center">
+            <h3 className="font-droid font-bold text-base inline-block px-2 py-1 font-semibold tracking-wide text-white bg-[var(--color-primary)] uppercase">
+              I. Introducción
+            </h3>
+            <div>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Cuando usas lolvi. nos confías tus datos personales. Nos comprometemos a mantener esa confianza. Para esto, comenzaremos ayudándolo a comprender nuestras prácticas de privacidad.
+              </p>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                En este aviso se describen los datos personales que recopilamos, cómo los usamos y compartimos, y sus opciones con respecto a ellos.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative z-10 py-12 px-6 sm:px-2 bg-transparent">
+        <div className="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-8 md:gap-y-10">
+          <div className="relative text-center">
+            <h3 className="mt-2 mb-2 font-droid font-bold text-base inline-block px-2 py-1 font-semibold tracking-wide text-white bg-[var(--color-primary)] uppercase">
+              II. Filosofía de trabajo de lolvi.
+            </h3>
+            <br />
+            <h4 className="font-droid font-bold text-base inline-block px-2 py-1 text-black font-semibold tracking-wide bg-gray-400 uppercase">
+              A. Alcance
+            </h4>
+            <div>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Este aviso se aplica a todos los usuarios de los servicios de lolvi. en Colombia, y de su app y web.
+              </p>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                En este aviso se describe cómo lolvi recopila y usa datos. Este aviso se aplica a todos los usuarios de lolvi.:
+              </p>
+            </div>
+
+            <div>
+              <dl className="font-droid mt-4 max-w-2xl text-base text-gray-500 text-center lg:mx-auto w-full inline-block">
+                <div className="mt-2 mb-2 w-full">
+                  <dt className="inline text-center font-bold">Usuarios: </dt>
+                  <dd className="inline ">
+                    Personas que solicitan o reciben el servicio de pago de servicios públicos o facturas a través de lolvi.-
+                  </dd>
+                </div>
+                <div className="mt-2 mb-2">
+                  <dt className="inline font-bold">Servicios Públicos: </dt>
+                  <dd className="inline">
+                    Recibos de los proveedores de servicios públicos o de cualquier factura recurrente de cada región y ciudad.
+                  </dd>
+                </div>
+                <div className="mt-2 mb-2">
+                  <dt className="inline font-bold">Agentes de servicio de lolvi.: </dt>
+                  <dd className="inline">
+                    Personas que prestan el servicio de pago de facturas a nombre de LO OLVIDE PAGAR S.A.S.
+                  </dd>
+                </div>
+                <div className="mt-2 mb-2">
+                  <dt className="inline font-bold">Agentes Intermediarios: </dt>
+                  <dd className="inline">
+                    Empresas procesadoras de pagos a través de los cuáles se procesan y pagan los servicios públicos o facturas.
+                  </dd>
+                </div>
+                <div className="mt-2 mb-2">
+                  <dt className="inline font-bold">Proveedores de Servicios Públicos: </dt>
+                  <dd className="inline">
+                    Empresas proveedoras de servicios públicos.
+                  </dd>
+                </div>
+              </dl>
+            </div>
+
+            <div className="w-full text-center">
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Se hará referencia a todas las personas sujetas a este aviso como “usuarios”.
+              </p>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Nuestras prácticas de privacidad están sujetas a las leyes vigentes en los lugares donde operamos. Esto significa que aplicamos las prácticas descritas en el presente aviso en Colombia.
+              </p>
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Contáctenos a través de las direcciones y números a continuación si tiene preguntas sobre nuestras prácticas en un lugar específico.
+              </p>
+              <div className="text-center">
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  <strong>Dirección</strong>: Cra 14b # 161-54 torre 6 apto 602 <br />
+                  <strong>Número de Contacto:</strong> 316 044 55 00
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full text-center">
+              <h4 className="mt-4 font-droid font-bold text-base inline-block px-2 py-1 text-black font-semibold tracking-wide bg-gray-400 uppercase">
+                B. Transferencia y responsable del tratamiento de datos
+              </h4>
+              <div className="text-center">
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  LO OLVIDE PAGAR S.A.S. son los responsables del tratamiento de los datos recopilados con relación al uso de los servicios de lolvi. en Colombia.
+                </p>
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  lolvi. opera y procesa datos personales de personas en Colombia que adquieran el servicio y registren sus recibos o facturas. Esto quiere decir que el registro de la información no se limita a nacionales Colombianos sino a las personas que inscriben esos servicios que pueden tener cualquier nacionalidad.
+                </p>
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  Esto significa que la información estará disponible para los usuarios incluso fuera del país si acceden a la aplicación móvil. Los usuarios pueden acceder a los historiales de información en cualquier momento y desde cualquier lugar donde la aplicación esté activa.
+                </p>
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  Esto lo hacemos para cumplir con los acuerdos con todos nuestros usuarios o conforme a sus instrucciones o consentimiento previo, u otros mecanismos de transferencia disponibles en virtud de la ley vigente, como las Cláusulas contractuales básicas.
+                </p>
+                <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                  Para su comodidad, el ejercicio de sus derechos de protección de datos puede dirigirse a lolvi a través de cualquiera de los canales designados mencionados anteriormente y se gestionará de manera centralizada en nombre del responsable del tratamiento correspondiente.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative z-10 py-12 px-6 sm:px-2 bg-transparent">
+        <div className="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-8 md:gap-y-10">
+          <div className="relative text-center">
+            <h3 className="mt-2 mb-2 font-droid font-bold text-base inline-block px-2 py-1 font-semibold tracking-wide text-white bg-[var(--color-primary)] uppercase">
+              III. Recopilación y usos de los datos
+            </h3>
+            <br />
+            <h4 className="font-droid font-bold text-base inline-block px-2 py-1 text-black font-semibold tracking-wide bg-gray-400 uppercase">
+              A. Datos recopilados
+            </h4>
+            <div className="text-center">
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                lolvi. recopila los siguientes datos:
+              </p>
+            </div>
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-decimal">
+                <li className="text-left">
+                  Datos que los usuarios proporcionan a lolvi, como los que brindan cuando crean sus cuentas.
+                </li>
+                <li className="text-left">
+                  Datos que se generan cuando se utilizan nuestros servicios, pagos, registro de facturas o recibos, proveedores de servicios y uso de la app.
+                </li>
+                <li className="text-left">
+                  Datos de otras fuentes, como procesadores de pagos o agentes intermediarios, socios comerciales, proveedores generales, de seguros o de soluciones financieras, y autoridades gubernamentales.
+                </li>
+              </ul>
+            </div>
+
+            <div className="text-center">
+              <p className="inline-block font-droid mt-8 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                1. Datos proporcionados por usuarios. Se incluye lo siguiente:
+              </p>
+            </div>
+
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-disc">
+                <li className="text-left">
+                  Información del perfil de usuario: Recopilamos datos cuando los usuarios crean o actualizan sus cuentas lolvi, lo que puede incluir su nombre, correo electrónico, número de teléfono, nombre de usuario y contraseña, dirección, detalles bancarios o de pago (incluidos los relacionados con su verificación de pago), y otros documentos de identificación oficiales (que pueden incluir el número de identificación, así como la fecha de nacimiento, género y foto).
+                </li>
+                <li className="text-left">
+                  Fotos para la verificación de identidad: Se incluyen fotos de los usuarios (por ejemplo, selfies) o sus identificaciones oficiales (por ejemplo, licencia de conducir o pasaporte). Estas fotos se pueden usar para verificar la identidad de un usuario, por ejemplo, a través de tecnologías de verificación facial. Consulte la sección “Cómo usamos los datos personales” para obtener más información sobre la manera en que se usan las fotos de los usuarios con fines de seguridad, incluida la verificación de identidad.
+                </li>
+                <li className="text-left">
+                  Datos demográficos: Podemos recopilar datos demográficos sobre los usuarios, como la fecha de nacimiento, la edad, el género y la ocupación, cuando sea necesario para ciertos servicios de lolvi.
+                </li>
+                <li className="text-left">
+                  Contenido del usuario: Recopilamos los datos que los usuarios envían cuando se contactan con el soporte al cliente de lolvi. (incluso en los Centros de atención o mediante funciones de chat telefónico o videoconferencia).
+                </li>
+              </ul>
+            </div>
+
+            <div className="text-center">
+              <p className="inline-block font-droid mt-8 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                2. Datos creados durante el uso de nuestros servicios. Se incluye lo siguiente:
+              </p>
+            </div>
+
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-disc">
+                <li className="text-left">
+                  Datos de facturas o servicios (usuarios): Recopilamos datos de número de pago de cada recibo de servicios públicos o factura, monto e historiales de pagos a través de la plataforma.
+                </li>
+                <li className="text-left">
+                  Datos de los proveedores de servicios (agentes intermediarios y proveedores de servicios): Recopilamos información de los canales de pago de las plataformas habilitadas para realizar los pagos de los servicios o facturas, las comisiones y tiempos de procesamiento.
+                </li>
+              </ul>
+            </div>
+
+            <div className="text-center">
+              <p className="inline-block font-droid mt-8 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Datos generados por el uso de nuestros servicios. Se incluye lo siguiente:
+              </p>
+              <p className="inline-block font-droid mt-8 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                Los usuarios pueden eliminar su información de la aplicación en cualquier momento, sin embargo esto afectará el funcionamiento futuro de cualquier pago de servicios o facturas.
+              </p>
+            </div>
+
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-disc">
+                <li className="text-left">
+                  Información de transacciones: Recopilamos información sobre las transacciones relacionadas con el uso de nuestros servicios, incluido el monto cargado o pagado a la aplicación, los detalles del pago y la fecha.
+                </li>
+                <li className="text-left">
+                  Datos de uso: Recopilamos información sobre cómo los usuarios interactúan con nuestros servicios.
+                </li>
+                <li className="text-left">
+                  Datos del dispositivo: Recopilamos datos sobre los dispositivos usados para acceder a nuestros servicios, lo que incluye modelos de hardware, dirección IP u otros identificadores únicos del dispositivo, sistemas operativos y versiones, software, idiomas preferidos, identificadores de publicidad, información de movimiento del dispositivo y datos de la red móvil.
+                </li>
+                <li className="text-left">
+                  Datos de comunicación: Recopilamos datos sobre las comunicaciones por teléfono, mensajes de texto entre los usuarios y el área de servicio al cliente.
+                </li>
+              </ul>
+            </div>
+
+            <div className="text-center">
+              <p className="inline-block font-droid mt-8 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                3. Datos de otras fuentes. Entre estos se incluyen los siguientes:
+              </p>
+            </div>
+
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-disc">
+                <li className="text-left">
+                  Socios comerciales de lolvi. a través de los cuales los cuales se realizan o procesan los pagos de los servicios ofrecidos por lolvi.
+                </li>
+                <li className="text-left">
+                  Socios comerciales de lolvi. relacionados con tarjetas de débito o de crédito emitidas por una institución financiera asociada con lolvi. en la medida en que se indique en los Términos y condiciones de la tarjeta.
+                </li>
+                <li className="text-left">Fuentes disponibles públicamente.</li>
+              </ul>
+            </div>
+            <div className="text-center">
+              <h4 className="inline-block font-droid mt-4 font-bold text-base inline-block px-2 py-1 text-black font-semibold tracking-wide bg-gray-400 uppercase">
+                B. Cómo usamos los datos personales
+              </h4>
+            </div>
+
+            <div className="text-center">
+              <p className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
+                lolvi. usa los datos para que el servicio de pago de recibos o servicios públicos sean confiables y convenientes. También usamos estos datos para lo siguiente:
+              </p>
+            </div>
+            <div className="text-center">
+              <ul className="inline-block font-droid mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto list-disc">
+                <li className="text-left">
+                  Optimizar los tiempos y los costos de los pagos de servicios públicos
+                </li>
+                <li className="text-left">Brindar soporte al cliente</li>
+                <li className="text-left">Realizar tareas de investigación y de desarrollo</li>
+                <li className="text-left">Permitir la comunicación entre usuarios</li>
+                <li className="text-left">Hacer campañas de marketing y publicidad</li>
+                <li className="text-left">Enviar comunicaciones no comerciales a los usuarios</li>
+                <li className="text-left">Abordar temas relacionados con procedimientos legales</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
